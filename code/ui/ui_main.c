5f6daead97b5f4f09383532a2c6913f18b375209
Zack Middleton <zturtleman@gmail.com>
2015-07-01 16:23:51 -0500

    Make resetting to defaults use cvar values in default.cfg
    Reset cvars before executing default.cfg instead of after.

--- code/ui/ui_main.c
+++ code/ui/ui_main.c
@@ -4973,8 +4973,8 @@ static void UI_RunMenuScript( char **args ) {
 			UI_GameType_HandleKey( 0, 0, K_MOUSE1, qfalse );
 			UI_GameType_HandleKey( 0, 0, K_MOUSE2, qfalse );
 		} else if ( Q_stricmp( name, "resetDefaults" ) == 0 ) {
-			trap_Cmd_ExecuteText( EXEC_APPEND, "exec default.cfg\n" );
 			trap_Cmd_ExecuteText( EXEC_APPEND, "cvar_restart\n" );
+			trap_Cmd_ExecuteText( EXEC_APPEND, "exec default.cfg\n" );
 			Controls_SetDefaults();
 			trap_Cvar_Set( "com_introPlayed", "1" );
 			trap_Cmd_ExecuteText( EXEC_APPEND, "vid_restart\n" );
